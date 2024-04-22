5e163bb4f7496b91047d6780831fd6fb5b2aeab9
moinkazi123 <155711429+moinkazi123@users.noreply.github.com>
2024-04-22 16:05:30 +0530

    new commit

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull moin7866/simple-python-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 moin7866/simple-python-app:latest
